ef7a6065ecd6b6a3c2a0e6f2d9589be42b56d4ec
Stephan Melzer <melzer.stephan@gmail.com>
2017-08-17 21:14:46 +0200

    Added recuse switch

--- Set-RandomWallpaper.ps1
+++ Set-RandomWallpaper.ps1
@@ -3,10 +3,12 @@ function Set-RandomWallpaper () {
         [Parameter(Mandatory = $true)]
         [string]
         $Path,
+        [Switch]
+        $Recurse,
         [ValidateSet("Tile", "Center", "Stretch", "Fit", "Fill")]
         [string]
         $Style = "Fill"
     )
-    $randomWallpaperPath = (Get-ChildItem  $Path -File | Where-Object { $_.Extension -in @(".jpg", ".png", ".bmp") } | Get-Random).FullName
+    $randomWallpaperPath = (Get-ChildItem -Recurse:$Recurse $Path -File | Where-Object { $_.Extension -in @(".jpg", ".png", ".bmp") } | Get-Random).FullName
     Set-Wallpaper -Path $randomWallpaperPath -Style $Style
 }
